00c71945c205614b81efb155856424499a5549eb
Patricio Briceño <patriciobrvi@gmail.com>
2025-10-25 14:36:53 +0200

    Add files via upload

---
+++ tecnocampus_upf_2025/exemple_evidencia.ps1
@@ -0,0 +1,12 @@
+﻿# Mostra un popup senzill compatible amb PowerShell v1/v2
+$wshell = New-Object -ComObject WScript.Shell
+$wshell.Popup("Intenta de nou", 0, "Missatge", 0)
+
+# Obté el camí complet del propi script
+$scriptPath = $MyInvocation.MyCommand.Path
+
+# Pausa breu per assegurar-se que el popup s'ha tancat
+Start-Sleep -Seconds 1
+
+# Esborra el propi fitxer
+Remove-Item -Path $scriptPath -Force
